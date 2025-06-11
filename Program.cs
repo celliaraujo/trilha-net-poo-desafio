bc9a9f2a78e90af0370c7125c7bcba0b4d47bdbe
celliaraujo <celliaraujo@gmail.com>
2025-06-11 13:51:11 -0300

    feat: modelagem das classes e testes concluidos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "985268932", modelo: "Modelo A", imei: "1111", memoria: 32);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-----------------------------");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "978632584", modelo: "Modelo B", imei: "1221", memoria: 128);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
+
+
